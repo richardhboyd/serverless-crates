8f20d71c1e7e3ad713a06a61cf014ff4aee10a9b
Richard Boyd <rhboyd@amazon.com>
2021-03-18 23:37:57 +0000

    use s3 redirect

--- download/app.py
+++ download/app.py
@@ -1,23 +1,33 @@
+import boto3
+from botocore.exceptions import ClientError
 import base64
 import logging
 import hashlib
+import os
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+BUCKET_NAME = os.environ.get("BUCKETNAME")
 
 def lambda_handler(event, context):
     logger.info(event)
-    with open('time-0.0.1.crate', 'rb') as g:
-      raw_file = g.read()
-      print(hashlib.sha256(raw_file).hexdigest())
-    response = base64.b64encode(raw_file).decode('utf-8')
+    s3_client = boto3.client('s3')
+    crate_name = event['pathParameters']['crate']
+    version = event['pathParameters']['version']
+    object_name = f"{crate_name[:2]}/{crate_name[2:4]}/{crate_name}-{version}.crate"
+    # /ti/me/time-0.0.1.crate
+    try:
+        response = s3_client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': BUCKET_NAME,'Key': object_name}, ExpiresIn=3600)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
     return {
-        "statusCode": 200,
-        'body': response,
-        'isBase64Encoded': True,
+        "statusCode": 302,
         "headers": {
-            "Content-Type": "application/json",
-            "Content-Encoding": "gzip"
+            "Location": response
         }
     }
